591484618b8ef597ce1d78306fb4bfd7a9ee3192
valeri.ivanchev@limechain.tech <valeri.ivanchev@limechain.tech>
2022-08-31 16:50:43 +0300

    Add subtask for printing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,21 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter";
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig, subtask, task } from "hardhat/config";
 
 task("deploy-testnets", "Deploys contract on a provided network")
   .setAction(async () => {
     const { deployLibrary } = require('./scripts/deploy');
     await deployLibrary();
   });
+
+subtask("print", "Prints valuble info")
+  .addParam("deployerAddress", "The deployer address")
+  .addParam("contractAddress", "The contractAddress address")
+  .setAction(async ({ deployerAddress, contractAddress }) => {
+    console.log(`Deployed contract with the account: ${deployerAddress}`);
+    console.log(`Deployed contract at address: ${contractAddress}`)
+  })
+
 
 const config: HardhatUserConfig = {
   solidity: {
